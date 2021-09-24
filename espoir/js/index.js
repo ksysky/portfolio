51bd90ef8376b046ff90caf0811defde27a3c9a5
ksysky <78029275+ksysky@users.noreply.github.com>
2021-09-25 01:16:25 +0900

    look book slide edit 2

--- espoir/js/index.js
+++ espoir/js/index.js
@@ -148,7 +148,7 @@ $(".box2 .pop_box_2").mouseleave(function () {
 
 // 룩북 반응형 모바일 슬라이드
 $('.lookbook_m').slick({
-	centerMode: true,
+	// centerMode: true,
 	// centerPadding: '8px',
 	slidesToShow: 1,
 	arrows: true
