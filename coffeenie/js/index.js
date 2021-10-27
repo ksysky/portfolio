f7bf765695406e618cfb4bb0fa2fb9258a1a579d
ksysky <78029275+ksysky@users.noreply.github.com>
2021-10-27 20:02:35 +0900

    ..

--- coffeenie/js/index.js
+++ coffeenie/js/index.js
@@ -134,7 +134,23 @@ $(".md_slide").slick({
 	autoplaySpeed: 2500,
 	speed: 2500,
 	dots: true,
-	arrows: false
+	arrows: false,
+	responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3
+		  }
+		},
+		{
+			breakpoint: 720,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 2,
+			}
+		}
+	]
 });
 
 $(".play").click(function(){
