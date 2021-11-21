fd069c9957b31908e08c4fd737e5e6dd65349072
ksysky <78029275+ksysky@users.noreply.github.com>
2021-11-22 01:58:37 +0900

    헤더 반응형

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,24 @@
 // 메인 텍스트 타이핑 효과
-const typingBool = false;
-const typingIdx = 0;
-const liIndex = 0;
-const pLength = $(".typing p").length;
+// const typingBool = false;
+// const typingIdx = 0;
+// const liIndex = 0;
+// const pLength = $(".typing p").length;
 
-const typingTxt = $(".typing p").eq(index).text();
-typingTxt = typingTxt.split("");
+// const typingTxt = $(".typing p").eq(index).text();
+// typingTxt = typingTxt.split("");
 
-if (typingBool == false) {
-    typingBool = true;
-    const tyInt = setInterval(typing,100);
-}
+// if (typingBool == false) {
+//     typingBool = true;
+//     const tyInt = setInterval(typing,100);
+// }
 
-function typing() {
-    $(".typing p").removeClass("on");
-    $(".typing p").eq(liIndex).addClass("on");
-    if (typingIdx < typingTxt.length) {
-        $(".typing > p").eq(liIndex).append(typingTxt[typingIdx]);
-        typingIdx++;
-    } else {
+// function typing() {
+//     $(".typing p").removeClass("on");
+//     $(".typing p").eq(liIndex).addClass("on");
+//     if (typingIdx < typingTxt.length) {
+//         $(".typing > p").eq(liIndex).append(typingTxt[typingIdx]);
+//         typingIdx++;
+//     } else {
 
-    }
-}
+//     }
+// }
