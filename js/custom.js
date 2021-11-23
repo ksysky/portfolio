98233667907d338b3040c5c1e7b2fe422fc17fa4
ksysky <78029275+ksysky@users.noreply.github.com>
2021-11-23 21:00:58 +0900

    타이핑

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(".mBtn").click(function() {
 });
 
 // 메인 타이핑 애니메이션
-const content = "안녕하세요. \n 신입 웹퍼블리셔 \n 김수연의 포트폴리오입니다."
+const content = "안녕하세요.\n신입 웹퍼블리셔\n김수연의 포트폴리오입니다."
 const text = document.querySelector(".t_text");
 let i = 0;
 
