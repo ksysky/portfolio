4108cd190c2df58d466f1b434723ea5505d7081c
ksysky <78029275+ksysky@users.noreply.github.com>
2021-11-23 18:21:07 +0900

    타이핑 효과

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,22 @@
-// 메인 텍스트 타이핑 효과
-// const typingBool = false;
-// const typingIdx = 0;
-// const liIndex = 0;
-// const pLength = $(".typing p").length;
-
-// const typingTxt = $(".typing p").eq(index).text();
-// typingTxt = typingTxt.split("");
-
-// if (typingBool == false) {
-//     typingBool = true;
-//     const tyInt = setInterval(typing,100);
-// }
-
-// function typing() {
-//     $(".typing p").removeClass("on");
-//     $(".typing p").eq(liIndex).addClass("on");
-//     if (typingIdx < typingTxt.length) {
-//         $(".typing > p").eq(liIndex).append(typingTxt[typingIdx]);
-//         typingIdx++;
-//     } else {
-
-//     }
-// }
-
 // 모바일 메뉴 버튼
 $(".mBtn").click(function() {
     $(".m_menu").toggleClass("on");
     $(".mBtn").toggleClass("on");
     $(".m_back").toggleClass("on");
 });
+
+// 메인 타이핑 애니메이션
+const content = "안녕하세요. \n 신입 웹퍼블리셔 \n 김수연의 포트폴리오입니다."
+const text = document.querySelector(".t_text");
+let i = 0;
+
+function typing() {
+    let txt = content[i++];
+    text.innerHTML += txt === "\n" ? "<br/>" : txt;
+    if(i > content.length) {
+        text.textContent = "";
+        i = 0;
+    }
+}
+
+setInterval(typing, 200);
