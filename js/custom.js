ca5b9e0dc7ccd5546cbad58e64b89dc66c6c90e3
ksysky <78029275+ksysky@users.noreply.github.com>
2021-12-01 17:59:56 +0900

    contact

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 // 모바일 메뉴 버튼
-$(".mBtn").click(function(e) {
-    e.preventDefault();
-    $(".m_menu").toggleClass("on");
-    $(".mBtn").toggleClass("on");
-    $(".m_back").toggleClass("on");
+$(".mBtn").click(function (e) {
+  e.preventDefault();
+  $(".m_menu").toggleClass("on");
+  $(".mBtn").toggleClass("on");
+  $(".m_back").toggleClass("on");
 });
 
 // 메인 타이핑 애니메이션
@@ -12,30 +12,30 @@ const text = document.querySelector(".t_text");
 let i = 0;
 
 function typing() {
-    if(i < content.length) {
-        let txt = content[i++]; // 0에서 끝 숫자까지
-        text.innerHTML += txt === "\n" ? "<br/>" : txt;
-    }
+  if (i < content.length) {
+    let txt = content[i++]; // 0에서 끝 숫자까지
+    text.innerHTML += txt === "\n" ? "<br/>" : txt;
+  }
 }
 setInterval(typing, 150);
 
 // 포트폴리오 슬라이드
 const swiper = new Swiper('.slider', {
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1024: {
+      navigation: false,
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      1024: {
-        navigation: false,
-      },
-    }
-  });
+  }
+});
