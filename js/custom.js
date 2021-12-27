8e512e94ea651727dfdc87c6fba4cc255c8d6677
ksysky <78029275+ksysky@users.noreply.github.com>
2021-12-27 20:33:36 +0900

    함수 수정

--- js/custom.js
+++ js/custom.js
@@ -106,11 +106,11 @@ $(".m_back").click(function (e) {
 // 메인 타이핑 애니메이션
 const content = "안녕하세요.\n신입 웹퍼블리셔\n김수연의 포트폴리오입니다."
 const text = document.querySelector(".t_text");
-let i = 0;
+var i = 0;
 
 function typing() {
   if (i < content.length) {
-    let txt = content[i++]; // 0에서 끝 숫자까지
+    var txt = content[i++]; // 0에서 끝 숫자까지
     text.innerHTML += txt === "\n" ? "<br/>" : txt;
   }
 }
