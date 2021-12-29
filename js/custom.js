bd7ef37d1f8f81a5faf38f2a55f9e29f9339a379
ksysky <78029275+ksysky@users.noreply.github.com>
2021-12-30 01:13:40 +0900

    되돌림

--- js/custom.js
+++ js/custom.js
@@ -92,18 +92,13 @@ var content = "안녕하세요.\n신입 웹퍼블리셔\n김수연의 포트폴�
 var text = document.querySelector(".t_text");
 var i = 0;
 
-// function typing() {
-//   if (i < content.length) {
-//     var txt = content[i++]; // 0에서 끝 숫자까지 0~30
-//     text.innerHTML += txt === "\n" ? "<br/>" : txt;
-//   }
-// }
-setInterval(function(){
+function typing() {
   if (i < content.length) {
     var txt = content[i++]; // 0에서 끝 숫자까지 0~30
     text.innerHTML += txt === "\n" ? "<br/>" : txt;
   }
-}, 150);
+}
+setInterval(typing, 150);
 
 // 포트폴리오 슬라이드
 var swiper = new Swiper('.slider', {
